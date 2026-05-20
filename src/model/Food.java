71e5c6d1c6f6f9e10f51734bddab58853042779f
Alex <vintalexandru03@gmail.com>
2026-05-20 21:48:55 +0300

    food class in model/

---
+++ src/model/Food.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Food {
+    public int x;
+    public int y;
+    public int size = 40;
+
+    public Food(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
